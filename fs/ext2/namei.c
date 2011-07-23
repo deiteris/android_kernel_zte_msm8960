ab9bfb23d9e4cc20cb0a96abe1474cee4d4f9b24
Christoph Hellwig <hch@lst.de>
2011-07-23 17:37:31 +0200

    fs: take the ACL checks to common code
    Replace the ->check_acl method with a ->get_acl method that simply reads an
    ACL from disk after having a cache miss.  This means we can replace the ACL
    checking boilerplate code with a single implementation in namei.c.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Conflicts:
    	fs/9p/acl.c
    	fs/jffs2/acl.c
    	fs/posix_acl.c
    	fs/reiserfs/xattr.c
    	fs/xfs/linux-2.6/xfs_acl.c

--- fs/ext2/namei.c
+++ fs/ext2/namei.c
@@ -412,7 +412,7 @@ const struct inode_operations ext2_dir_inode_operations = {
 	.removexattr	= generic_removexattr,
 #endif
 	.setattr	= ext2_setattr,
-	.check_acl	= ext2_check_acl,
+	.get_acl	= ext2_get_acl,
 };
 
 const struct inode_operations ext2_special_inode_operations = {
@@ -423,5 +423,5 @@ const struct inode_operations ext2_special_inode_operations = {
 	.removexattr	= generic_removexattr,
 #endif
 	.setattr	= ext2_setattr,
-	.check_acl	= ext2_check_acl,
+	.get_acl	= ext2_get_acl,
 };
