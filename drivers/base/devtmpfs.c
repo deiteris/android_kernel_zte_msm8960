f5c3cd206d90734e72031810dbfad7ae9461a996
Al Viro <viro@zeniv.linux.org.uk>
2011-07-23 20:24:48 -0400

    switch device_get_devnode() and ->devnode() to umode_t *
    both callers of device_get_devnode() are only interested in lower 16bits
    and nobody tries to return anything wider than 16bit anyway.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Conflicts:
    	arch/x86/kernel/cpuid.c
    	arch/x86/kernel/msr.c
    	drivers/base/devtmpfs.c
    	drivers/char/tile-srom.c
    	drivers/media/dvb/ddbridge/ddbridge-core.c

--- drivers/base/devtmpfs.c
+++ drivers/base/devtmpfs.c
@@ -34,7 +34,20 @@ static int mount_dev = 1;
 static int mount_dev;
 #endif
 
+<<<<<<< HEAD
 static DEFINE_MUTEX(dirlock);
+=======
+static DEFINE_SPINLOCK(req_lock);
+
+static struct req {
+	struct req *next;
+	struct completion done;
+	int err;
+	const char *name;
+	umode_t mode;	/* 0 => delete */
+	struct device *dev;
+} *requests;
+>>>>>>> 2c9ede5... switch device_get_devnode() and ->devnode() to umode_t *
 
 static int __init mount_param(char *str)
 {
