10be9fd202a9a449d498d757eb49ef3bc4285cdc
Dima Zavin <dima@android.com>
2011-11-09 16:10:57 -0800

    ARM: common: fiq_debugger: add irq context debug functions
    This code is moved here from the drivers/misc/kernel_debugger.
    
    Change-Id: Iccf21c4313a8516a917125ca93f64baa5f354228
    Signed-off-by: Dima Zavin <dima@android.com>
    
    Conflicts:
    	arch/arm/common/fiq_debugger.c

--- arch/arm/common/fiq_debugger.c
+++ arch/arm/common/fiq_debugger.c
@@ -497,6 +497,7 @@ static void do_ps(struct fiq_debugger_state *state)
 	read_unlock(&tasklist_lock);
 }
 
+<<<<<<< HEAD
 #ifdef CONFIG_FIQ_DEBUGGER_CONSOLE
 static void begin_syslog_dump(struct fiq_debugger_state *state)
 {
@@ -515,11 +516,20 @@ static void begin_syslog_dump(struct fiq_debugger_state *state)
 }
 
 static void end_syslog_dump(struct fiq_debugger_state *state)
+=======
+extern int do_syslog(int type, char __user *bug, int count);
+static void do_sysrq(struct fiq_debugger_state *state, char rq)
+>>>>>>> b11ab5b... ARM: common: fiq_debugger: add irq context debug functions
 {
 	char buf[128];
 	int ret;
 	int idx = 0;
+<<<<<<< HEAD
 
+=======
+	do_syslog(5 /* clear */, NULL, 0);
+	handle_sysrq(rq);
+>>>>>>> b11ab5b... ARM: common: fiq_debugger: add irq context debug functions
 	while (1) {
 		ret = log_buf_copy(buf, idx, sizeof(buf) - 1);
 		if (ret <= 0)
@@ -529,6 +539,7 @@ static void end_syslog_dump(struct fiq_debugger_state *state)
 		idx += ret;
 	}
 }
+<<<<<<< HEAD
 #endif
 
 static void do_sysrq(struct fiq_debugger_state *state, char rq)
@@ -537,6 +548,8 @@ static void do_sysrq(struct fiq_debugger_state *state, char rq)
 	handle_sysrq(rq);
 	end_syslog_dump(state);
 }
+=======
+>>>>>>> b11ab5b... ARM: common: fiq_debugger: add irq context debug functions
 
 /* This function CANNOT be called in FIQ context */
 static void debug_irq_exec(struct fiq_debugger_state *state, char *cmd)
