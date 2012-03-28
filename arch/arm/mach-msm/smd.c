8e195640bbd54dd6748caa3e0a536a2b2e6a496f
Eric Holmberg <eholmber@codeaurora.org>
2012-03-28 14:01:02 -0600

    msm: smd: Do not use wakelock for local DEM notifications
    Notify local SMSM callback clients without using a wakelock since SMSM
    signaling is used by power management during power-down/-up sequencing
    on DEM-based targets.  Grabbing a wakelock in this case will abort
    power-down sequencing.
    
    Change-Id: I08de41480a9713f8ac52b563c7df8213d465466d
    Signed-off-by: Eric Holmberg <eholmber@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/smd.c

--- arch/arm/mach-msm/smd.c
+++ arch/arm/mach-msm/smd.c
@@ -2517,13 +2517,23 @@ restore_snapshot_count:
 		spin_unlock_irqrestore(&smsm_snapshot_count_lock, flags);
 	}
 
-	spin_lock_irqsave(&smsm_snapshot_count_lock, flags);
-	if (smsm_snapshot_count == 0) {
-		SMx_POWER_INFO("SMSM snapshot wake lock\n");
-		wake_lock(&smsm_snapshot_wakelock);
+	/* queue wakelock usage flag */
+	ret = kfifo_in(&smsm_snapshot_fifo,
+			&use_wakelock, sizeof(use_wakelock));
+	if (ret != sizeof(use_wakelock)) {
+		pr_err("%s: SMSM snapshot failure %d\n", __func__, ret);
+		return;
 	}
-	++smsm_snapshot_count;
-	spin_unlock_irqrestore(&smsm_snapshot_count_lock, flags);
+
+	if (use_wakelock) {
+		spin_lock_irqsave(&smsm_snapshot_count_lock, flags);
+		if (smsm_snapshot_count == 0) {
+			SMx_POWER_INFO("SMSM snapshot wake lock\n");
+			wake_lock(&smsm_snapshot_wakelock);
+		}
+		++smsm_snapshot_count;
+		spin_unlock_irqrestore(&smsm_snapshot_count_lock, flags);
+	}
 	schedule_work(&smsm_cb_work);
 }
 
@@ -2769,7 +2779,6 @@ void notify_smsm_cb_clients_worker(struct work_struct *work)
 	uint32_t use_wakelock;
 	int ret;
 	unsigned long flags;
-	int snapshot_size = SMSM_NUM_ENTRIES * sizeof(uint32_t);
 
 	if (!smd_initialized)
 		return;
@@ -2816,17 +2825,31 @@ void notify_smsm_cb_clients_worker(struct work_struct *work)
 		}
 		mutex_unlock(&smsm_lock);
 
-		spin_lock_irqsave(&smsm_snapshot_count_lock, flags);
-		if (smsm_snapshot_count) {
-			--smsm_snapshot_count;
-			if (smsm_snapshot_count == 0) {
-				SMx_POWER_INFO("SMSM snapshot wake unlock\n");
-				wake_unlock(&smsm_snapshot_wakelock);
-			}
-		} else {
-			pr_err("%s: invalid snapshot count\n", __func__);
+		/* read wakelock flag */
+		ret = kfifo_out(&smsm_snapshot_fifo, &use_wakelock,
+				sizeof(use_wakelock));
+		if (ret != sizeof(use_wakelock)) {
+			pr_err("%s: snapshot underflow %d\n",
+				__func__, ret);
+			return;
 		}
-		spin_unlock_irqrestore(&smsm_snapshot_count_lock, flags);
+
+		if (use_wakelock) {
+			spin_lock_irqsave(&smsm_snapshot_count_lock, flags);
+			if (smsm_snapshot_count) {
+				--smsm_snapshot_count;
+				if (smsm_snapshot_count == 0) {
+					SMx_POWER_INFO("SMSM snapshot"
+						   " wake unlock\n");
+					wake_unlock(&smsm_snapshot_wakelock);
+				}
+			} else {
+				pr_err("%s: invalid snapshot count\n",
+						__func__);
+			}
+			spin_unlock_irqrestore(&smsm_snapshot_count_lock,
+					flags);
+		}
 	}
 }
 
