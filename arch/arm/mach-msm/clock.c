5a603f04ce7efafcf91acee757d69107ecc86e16
Matt Wagantall <mattw@codeaurora.org>
2012-04-03 11:00:56 -0700

    msm: clock: Clean up handoff function return values
    The intended interpretation of handoff function return value was for 1 or
    true to indicate that a handoff of a running clock had been successfully
    performed, and 0 or false to indicate that it had not (either because the
    clock was off or the rate was invalid).
    
    Some handoff functions added later, however, used the value of 0 to mean
    something else: that the handoff of some state was successful, but the
    enabled/disabled state of the clock was not checked.
    
    The vague definitions for the return codes of these functions make
    it difficult to understand their intended behaviour. Address this by
    using an enum of return codes so that their intentions are obvious.
    Also update the functions that did not check enabled/disabled state
    to do so and return meaningful values.
    
    Change-Id: Iebf4c9e4b358379f7f048a720888b299dc9ee8e5
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/clock-pll.c

--- arch/arm/mach-msm/clock.c
+++ arch/arm/mach-msm/clock.c
@@ -373,7 +373,7 @@ void __init msm_clock_init(struct clock_init_data *data)
 		clk_log_map_register(clk);
 		clk_set_parent(clk, parent);
 		if (clk->ops->handoff && !(clk->flags & CLKFLAG_HANDOFF_RATE)) {
-			if (clk->ops->handoff(clk)) {
+			if (clk->ops->handoff(clk) == HANDOFF_ENABLED_CLK) {
 				clk->flags |= CLKFLAG_HANDOFF_RATE;
 				clk_enable(clk);
 			}
