20d9754dfe4faf5b5a7f282806e3487ca04831f8
Naveen Ramaraj <nramaraj@codeaurora.org>
2012-04-09 15:58:40 -0700

    msm: ocmem: Support for On-Chip Memory
    Add initial support for on-chip memory present in copper.
    This patch adds basic driver infrastructure that sets up zones.
    
    Change-Id: I520d67dbf2178eef4ddb81799d5901d31e9976e4
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>
    
    Conflicts:
    	arch/arm/boot/dts/msmcopper.dtsi

--- drivers/video/msm/vidc/common/dec/vdec.c
+++ drivers/video/msm/vidc/common/dec/vdec.c
@@ -2108,8 +2108,8 @@ int vid_dec_open_client(struct video_client_ctx **vid_clnt_ctx, int flags)
 	}
 
 	client_index = vid_dec_get_empty_client_index();
-	if (client_index == -1) {
-		ERR("%s() : No free clients client_index == -1\n", __func__);
+	if (client_index < 0) {
+		ERR("%s() : No free clients client_index < 0\n", __func__);
 		rc = -ENOMEM;
 		goto client_failure;
 	}
