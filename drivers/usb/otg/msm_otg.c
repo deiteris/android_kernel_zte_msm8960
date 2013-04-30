54e94540f34d6960aded623d695742be9297fbba
motley-git <motley.slate@gmail.com>
2013-04-30 18:40:05 +0200

    USB force fast charging
    -Use base from Chad Froebel <chadfroebel@gmail.com> so we continue to
    use the same generic sysfs mechanism
    
    -Referenced franco's git for drivers/usb/otg/msm_otg.c changes
    https://github.com/franciscofranco/mako/commit/5a8a4c8e23c7e1df39c740b9ce1d5dda20b0ede3
    
    Off by default
    
    Turn on:
    echo 1 > /sys/kernel/fast_charge/force_fast_charge
    
    Turn off:
    echo 0 > /sys/kernel/fast_charge/force_fast_charge
    
    Conflicts:
    	arch/arm/mach-msm/Kconfig
    	arch/arm/mach-msm/Makefile
    	drivers/usb/otg/msm_otg.c

--- drivers/usb/otg/msm_otg.c
+++ drivers/usb/otg/msm_otg.c
@@ -44,6 +44,11 @@
 #include <mach/msm_xo.h>
 #include <mach/msm_bus.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#define USB_FASTCHG_LOAD 1000 /* uA */
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -1022,8 +1027,20 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 
 	if (motg->cur_power == mA)
 		return;
+<<<<<<< HEAD
 
 	dev_info(motg->otg.dev, "Avail curr from USB = %u\n", mA);
+=======
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge == 1) {
+			mA = USB_FASTCHG_LOAD;
+			pr_info("USB fast charging is ON - 1000mA.\n");
+	} else {
+		pr_info("USB fast charging is OFF.\n");
+	}
+#endif
+	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
+>>>>>>> 381496d... USB force fast charging
 
 	/*
 	 *  Use Power Supply API if supported, otherwise fallback
@@ -3107,3 +3124,4 @@ module_exit(msm_otg_exit);
 
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("MSM USB transceiver driver");
+
