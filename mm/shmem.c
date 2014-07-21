8779eb64851e940ced40c87ba83536e511e8593a
RIP95 <rip95_95@mail.ru>
2014-07-21 11:43:32 +0400

    Revert "Revert "fs: take the ACL checks to common code""
    This reverts commit b3288725595b2113fd39d569a9500f92ad032fcf.

--- mm/shmem.c
+++ mm/shmem.c
@@ -2723,10 +2723,6 @@ static const struct inode_operations shmem_inode_operations = {
 	.listxattr	= shmem_listxattr,
 	.removexattr	= shmem_removexattr,
 #endif
-#ifdef CONFIG_TMPFS_POSIX_ACL
-	.check_acl	= generic_check_acl,
-#endif
-
 };
 
 static const struct inode_operations shmem_dir_inode_operations = {
@@ -2749,7 +2745,6 @@ static const struct inode_operations shmem_dir_inode_operations = {
 #endif
 #ifdef CONFIG_TMPFS_POSIX_ACL
 	.setattr	= shmem_setattr,
-	.check_acl	= generic_check_acl,
 #endif
 };
 
@@ -2762,7 +2757,6 @@ static const struct inode_operations shmem_special_inode_operations = {
 #endif
 #ifdef CONFIG_TMPFS_POSIX_ACL
 	.setattr	= shmem_setattr,
-	.check_acl	= generic_check_acl,
 #endif
 };
 
