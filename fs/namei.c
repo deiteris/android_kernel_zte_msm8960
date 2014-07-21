7f14d31ca94098cfcf3a60730fc26933129eeef6
RIP95 <rip95_95@mail.ru>
2014-07-21 11:39:22 +0400

    Revert "Revert "->permission() sanitizing: pass MAY_NOT_BLOCK to ->check_acl()""
    This reverts commit 0280e9770d02d203bce136a4b64204e08dbb4c19.

--- fs/namei.c
+++ fs/namei.c
@@ -181,7 +181,7 @@ static int acl_permission_check(struct inode *inode, int mask, unsigned int flag
 {
 	unsigned int mode = inode->i_mode;
 
-	mask &= MAY_READ | MAY_WRITE | MAY_EXEC;
+	mask &= MAY_READ | MAY_WRITE | MAY_EXEC | MAY_NOT_BLOCK;
 
 	if (current_user_ns() != inode_userns(inode))
 		goto other_perms;
@@ -203,7 +203,7 @@ other_perms:
 	/*
 	 * If the DACs are ok we don't need any capability check.
 	 */
-	if ((mask & ~mode) == 0)
+	if ((mask & ~mode & (MAY_READ | MAY_WRITE | MAY_EXEC)) == 0)
 		return 0;
 	return -EACCES;
 }
