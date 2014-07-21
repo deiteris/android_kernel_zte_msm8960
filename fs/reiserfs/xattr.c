6a8daebadbb83112c366000f8d012eef0dfa5e33
RIP95 <rip95_95@mail.ru>
2014-07-21 11:16:33 +0400

    Revert "kill check_acl callback of generic_permission()"
    This reverts commit 3cb3319a8bfd5a9ccc329d02425e0c172b6d9ef4.

--- fs/reiserfs/xattr.c
+++ fs/reiserfs/xattr.c
@@ -868,16 +868,10 @@ out:
 	return err;
 }
 
-int reiserfs_check_acl(struct inode *inode, int mask, unsigned int flags)
+static int reiserfs_check_acl(struct inode *inode, int mask, unsigned int flags)
 {
 	struct posix_acl *acl;
 	int error = -EAGAIN; /* do regular unix permission checks by default */
-
-	/*
-	 * Stat data v1 doesn't support ACLs.
-	 */
-	if (get_inode_sd_version(inode) == STAT_DATA_V1)
-		return -EAGAIN;
 
 	if (flags & IPERM_FLAG_RCU)
 		return -ECHILD;
@@ -967,7 +961,15 @@ int reiserfs_permission(struct inode *inode, int mask, unsigned int flags)
 	if (IS_PRIVATE(inode))
 		return 0;
 
-	return generic_permission(inode, mask, flags);
+#ifdef CONFIG_REISERFS_FS_XATTR
+	/*
+	 * Stat data v1 doesn't support ACLs.
+	 */
+	if (get_inode_sd_version(inode) != STAT_DATA_V1)
+		return generic_permission(inode, mask, flags,
+					reiserfs_check_acl);
+#endif
+	return generic_permission(inode, mask, flags, NULL);
 }
 
 static int xattr_hide_revalidate(struct dentry *dentry, struct nameidata *nd)
