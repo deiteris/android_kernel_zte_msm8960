df0d27f0c923929c8f1ab5776d28e549107986a6
RIP95 <rip95_95@mail.ru>
2014-07-21 11:26:10 +0400

    Revert "fs: take the ACL checks to common code"
    This reverts commit 576078786a02e77dc49c0f8480fea9365b159f94.

--- fs/ext4/file.c
+++ fs/ext4/file.c
@@ -280,7 +280,7 @@ const struct inode_operations ext4_file_inode_operations = {
 	.listxattr	= ext4_listxattr,
 	.removexattr	= generic_removexattr,
 #endif
-	.get_acl	= ext4_get_acl,
+	.check_acl	= ext4_check_acl,
 	.fiemap		= ext4_fiemap,
 };
 
