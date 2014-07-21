7c5d2a992f8c2632e55588273e7b1968c0d24202
RIP95 <rip95_95@mail.ru>
2014-07-21 11:36:42 +0400

    Revert "fs: add SEEK_HOLE and SEEK_DATA flags"
    This reverts commit ee82522db4626397e63567dc22177550af0732ec.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -32,9 +32,7 @@
 #define SEEK_SET	0	/* seek relative to beginning of file */
 #define SEEK_CUR	1	/* seek relative to current file position */
 #define SEEK_END	2	/* seek relative to end of file */
-#define SEEK_DATA	3	/* seek to the next data */
-#define SEEK_HOLE	4	/* seek to the next hole */
-#define SEEK_MAX	SEEK_HOLE
+#define SEEK_MAX	SEEK_END
 
 struct fstrim_range {
 	__u64 start;
