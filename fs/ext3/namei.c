fa51a120fb6f064eee5fefd20203ffe26d91eee2
RIP95 <rip95_95@mail.ru>
2014-07-21 11:35:18 +0400

    Revert "fs: take the ACL checks to common code"
    This reverts commit ab9bfb23d9e4cc20cb0a96abe1474cee4d4f9b24.

--- fs/ext3/namei.c
+++ fs/ext3/namei.c
@@ -2535,7 +2535,7 @@ const struct inode_operations ext3_dir_inode_operations = {
 	.listxattr	= ext3_listxattr,
 	.removexattr	= generic_removexattr,
 #endif
-	.get_acl	= ext3_get_acl,
+	.check_acl	= ext3_check_acl,
 };
 
 const struct inode_operations ext3_special_inode_operations = {
@@ -2546,5 +2546,5 @@ const struct inode_operations ext3_special_inode_operations = {
 	.listxattr	= ext3_listxattr,
 	.removexattr	= generic_removexattr,
 #endif
-	.get_acl	= ext3_get_acl,
+	.check_acl	= ext3_check_acl,
 };
