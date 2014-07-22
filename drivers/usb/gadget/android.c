245d519a2b36f135400834ac51734ea55c6aebde
RIP95 <rip95_95@mail.ru>
2014-07-22 23:18:45 +0400

    Revert "usb: gadget: android: Don't allow changing the functions list if enabled"
    This reverts commit 56cc02e0fca3fd9060aeca264d1a18e9abecf4bd.

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -121,7 +121,6 @@ struct android_dev {
 	struct android_usb_platform_data *pdata;
 
 	bool enabled;
-	struct mutex mutex;
 	bool connected;
 	bool sw_connected;
 	struct work_struct work;
@@ -1344,17 +1343,8 @@ functions_show(struct device *pdev, struct device_attribute *attr, char *buf)
 	struct android_usb_function *f;
 	char *buff = buf;
 
-	mutex_lock(&dev->mutex);
-
 	list_for_each_entry(f, &dev->enabled_functions, enabled_list)
-<<<<<<< HEAD
 		buff += snprintf(buff, PAGE_SIZE, "%s,", f->name);
-=======
-		buff += sprintf(buff, "%s,", f->name);
-
-	mutex_unlock(&dev->mutex);
-
->>>>>>> dc1b634... usb: gadget: android: Don't allow changing the functions list if enabled
 	if (buff != buf)
 		*(buff-1) = '\n';
 	return buff - buf;
@@ -1369,13 +1359,6 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 	char buf[256], *b;
 	int err;
 
-	mutex_lock(&dev->mutex);
-
-	if (dev->enabled) {
-		mutex_unlock(&dev->mutex);
-		return -EBUSY;
-	}
-
 	INIT_LIST_HEAD(&dev->enabled_functions);
 
 	strlcpy(buf, buff, sizeof(buf));
@@ -1389,8 +1372,6 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 				pr_err("android_usb: Cannot enable '%s'", name);
 		}
 	}
-
-	mutex_unlock(&dev->mutex);
 
 	return size;
 }
@@ -1408,8 +1389,6 @@ static ssize_t enable_store(struct device *pdev, struct device_attribute *attr,
 	struct android_dev *dev = dev_get_drvdata(pdev);
 	struct usb_composite_dev *cdev = dev->cdev;
 	int enabled = 0;
-
-	mutex_lock(&dev->mutex);
 
 	sscanf(buff, "%d", &enabled);
 	if (enabled && !dev->enabled) {
@@ -1441,8 +1420,6 @@ static ssize_t enable_store(struct device *pdev, struct device_attribute *attr,
 		pr_err("android_usb: already %s\n",
 				dev->enabled ? "enabled" : "disabled");
 	}
-
-	mutex_unlock(&dev->mutex);
 	return size;
 }
 
@@ -1623,9 +1600,9 @@ field ## _show(struct device *dev, struct device_attribute *attr,	\
 }									\
 static ssize_t								\
 field ## _store(struct device *dev, struct device_attribute *attr,	\
-		const char *buf, size_t size)				\
+		const char *buf, size_t size)		       		\
 {									\
-	int value;							\
+	int value;					       		\
 	if (sscanf(buf, format_string, &value) == 1) {			\
 		device_desc.field = value;				\
 		return size;						\
@@ -1643,14 +1620,10 @@ field ## _show(struct device *dev, struct device_attribute *attr,	\
 }									\
 static ssize_t								\
 field ## _store(struct device *dev, struct device_attribute *attr,	\
-		const char *buf, size_t size)				\
+		const char *buf, size_t size)		       		\
 {									\
 	if (size >= sizeof(buffer)) return -EINVAL;			\
-<<<<<<< HEAD
 	if (sscanf(buf, "%255s", buffer) == 1) {			\
-=======
-	if (sscanf(buf, "%s", buffer) == 1) {				\
->>>>>>> dc1b634... usb: gadget: android: Don't allow changing the functions list if enabled
 		return size;						\
 	}								\
 	return -1;							\
@@ -1929,7 +1902,6 @@ static int __init init(void)
 	dev->functions = supported_functions;
 	INIT_LIST_HEAD(&dev->enabled_functions);
 	INIT_WORK(&dev->work, android_work);
-	mutex_init(&dev->mutex);
 
 	ret = android_create_device(dev);
 	if (ret) {
