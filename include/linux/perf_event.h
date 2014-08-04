6a5b87be632c9f93fc3a098c8759584ddc2adf18
RIP95 <rip95_95@mail.ru>
2014-08-04 19:20:24 +0400

    Revert "perf: Fix loss of notification with multi-event"
    This reverts commit bfba880023c9daef7dd8821ae658fc0e1bd9056d.

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -813,7 +813,6 @@ struct perf_event {
 	int				mmap_locked;
 	struct user_struct		*mmap_user;
 	struct ring_buffer		*rb;
-	struct list_head		rb_entry;
 
 	/* poll related */
 	wait_queue_head_t		waitq;
