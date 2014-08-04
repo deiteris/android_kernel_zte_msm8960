fc1057975d8502b3a9bcb735fb96e36b978888e6
RIP95 <rip95_95@mail.ru>
2014-08-04 22:19:55 +0400

    Revert "scripts/kconfig/nconf: dynamically alloc dialog_input_result"
    This reverts commit 61b9e3a989136f5b239441d359350ba88d6d4d10.

--- scripts/kconfig/nconf.c
+++ scripts/kconfig/nconf.c
@@ -278,9 +278,6 @@ static int global_exit;
 /* the currently selected button */
 const char *current_instructions = menu_instructions;
 
-static char *dialog_input_result;
-static int dialog_input_result_len;
-
 static void conf(struct menu *menu);
 static void conf_choice(struct menu *menu);
 static void conf_string(struct menu *menu);
@@ -696,6 +693,7 @@ static void search_conf(void)
 {
 	struct symbol **sym_arr;
 	struct gstr res;
+	char dialog_input_result[100];
 	char *dialog_input;
 	int dres;
 again:
@@ -703,7 +701,7 @@ again:
 			_("Search Configuration Parameter"),
 			_("Enter " CONFIG_ " (sub)string to search for "
 				"(with or without \"" CONFIG_ "\")"),
-			"", &dialog_input_result, &dialog_input_result_len);
+			"", dialog_input_result, 99);
 	switch (dres) {
 	case 0:
 		break;
@@ -1360,6 +1358,7 @@ static void conf_choice(struct menu *menu)
 static void conf_string(struct menu *menu)
 {
 	const char *prompt = menu_get_prompt(menu);
+	char dialog_input_result[256];
 
 	while (1) {
 		int res;
@@ -1382,8 +1381,8 @@ static void conf_string(struct menu *menu)
 				prompt ? _(prompt) : _("Main Menu"),
 				heading,
 				sym_get_string_value(menu->sym),
-				&dialog_input_result,
-				&dialog_input_result_len);
+				dialog_input_result,
+				sizeof(dialog_input_result));
 		switch (res) {
 		case 0:
 			if (sym_set_string_value(menu->sym,
@@ -1403,13 +1402,14 @@ static void conf_string(struct menu *menu)
 
 static void conf_load(void)
 {
+	char dialog_input_result[256];
 	while (1) {
 		int res;
 		res = dialog_inputbox(main_window,
 				NULL, load_config_text,
 				filename,
-				&dialog_input_result,
-				&dialog_input_result_len);
+				dialog_input_result,
+				sizeof(dialog_input_result));
 		switch (res) {
 		case 0:
 			if (!dialog_input_result[0])
@@ -1434,13 +1434,14 @@ static void conf_load(void)
 
 static void conf_save(void)
 {
+	char dialog_input_result[256];
 	while (1) {
 		int res;
 		res = dialog_inputbox(main_window,
 				NULL, save_config_text,
 				filename,
-				&dialog_input_result,
-				&dialog_input_result_len);
+				dialog_input_result,
+				sizeof(dialog_input_result));
 		switch (res) {
 		case 0:
 			if (!dialog_input_result[0])
