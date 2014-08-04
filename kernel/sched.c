6855d33f093f6dda49e2a4c318e3b72603d60785
RIP95 <rip95_95@mail.ru>
2014-08-04 19:31:07 +0400

    Revert "sched: Set the command name of the idle tasks in SMP kernels"
    This reverts commit dea85d1aa3d7f72a786dcdd738e3dd3d221c22be.

--- kernel/sched.c
+++ kernel/sched.c
@@ -72,7 +72,6 @@
 #include <linux/ftrace.h>
 #include <linux/slab.h>
 #include <linux/cpuacct.h>
-#include <linux/init_task.h>
 
 #include <asm/tlb.h>
 #include <asm/irq_regs.h>
@@ -5991,9 +5990,6 @@ void __cpuinit init_idle(struct task_struct *idle, int cpu)
 	 */
 	idle->sched_class = &idle_sched_class;
 	ftrace_graph_init_idle_task(idle, cpu);
-#if defined(CONFIG_SMP)
-	sprintf(idle->comm, "%s/%d", INIT_TASK_COMM, cpu);
-#endif
 }
 
 /*
