a3a2ae4d743ad6ffea81f75794fb30b258914931
RIP95 <rip95_95@mail.ru>
2014-08-04 19:30:43 +0400

    Revert "Revert "Merge branch 'nf-next' of git://1984.lsi.us.es/net-next""
    This reverts commit c7280a68974f060606b172c8337c61cd4fc9f25b.

--- include/net/netfilter/nf_nat.h
+++ include/net/netfilter/nf_nat.h
@@ -1,13 +1,15 @@
 #ifndef _NF_NAT_H
 #define _NF_NAT_H
 #include <linux/netfilter_ipv4.h>
+<<<<<<< HEAD
+=======
+#include <linux/netfilter/nf_nat.h>
+>>>>>>> c5e1fd8... Merge branch 'nf-next' of git://1984.lsi.us.es/net-next
 #include <net/netfilter/nf_conntrack_tuple.h>
 
-#define NF_NAT_MAPPING_TYPE_MAX_NAMELEN 16
-
 enum nf_nat_manip_type {
-	IP_NAT_MANIP_SRC,
-	IP_NAT_MANIP_DST
+	NF_NAT_MANIP_SRC,
+	NF_NAT_MANIP_DST
 };
 
 /* SRC manip occurs POST_ROUTING or LOCAL_IN */
@@ -76,7 +78,7 @@ struct nf_conn_nat {
 
 /* Set up the info structure to map into this range. */
 extern unsigned int nf_nat_setup_info(struct nf_conn *ct,
-				      const struct nf_nat_range *range,
+				      const struct nf_nat_ipv4_range *range,
 				      enum nf_nat_manip_type maniptype);
 
 /* Is this tuple already taken? (not by us)*/
