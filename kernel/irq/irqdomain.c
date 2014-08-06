b42a4aa0d94ce0609ee0f0f90fee0eaafe241343
RIP95 <rip95_95@mail.ru>
2014-08-06 15:55:59 +0400

    Revert "ARM: 7235/1: irqdomain: export irq_domain_simple_ops for !CONFIG_OF"
    This reverts commit c55281243952ec0e97a22ae079de8f0623f7e32e.

--- kernel/irq/irqdomain.c
+++ kernel/irq/irqdomain.c
@@ -143,6 +143,11 @@ int irq_domain_simple_dt_translate(struct irq_domain *d,
 	return 0;
 }
 
+struct irq_domain_ops irq_domain_simple_ops = {
+	.dt_translate = irq_domain_simple_dt_translate,
+};
+EXPORT_SYMBOL_GPL(irq_domain_simple_ops);
+
 /**
  * irq_domain_create_simple() - Set up a 'simple' translation range
  */
@@ -177,10 +182,3 @@ void irq_domain_generate_simple(const struct of_device_id *match,
 }
 EXPORT_SYMBOL_GPL(irq_domain_generate_simple);
 #endif /* CONFIG_OF_IRQ */
-
-struct irq_domain_ops irq_domain_simple_ops = {
-#ifdef CONFIG_OF_IRQ
-	.dt_translate = irq_domain_simple_dt_translate,
-#endif /* CONFIG_OF_IRQ */
-};
-EXPORT_SYMBOL_GPL(irq_domain_simple_ops);
