3ad16176d43ab8174f3e405735fc0530b20ce799
RIP95 <rip95_95@mail.ru>
2014-08-07 15:06:53 +0400

    Ramdisk: fixups and cleanups.

--- ramdisk/init.qcom.sh
+++ ramdisk/init.qcom.sh
@@ -1,42 +1,5 @@
 #!/system/bin/sh
-# Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#     * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#     * Neither the name of Code Aurora nor
-#       the names of its contributors may be used to endorse or promote
-#       products derived from this software without specific prior written
-#       permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NON-INFRINGEMENT ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
-# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
-# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-
 target=`getprop ro.board.platform`
-
-#
-# It's unnessesary to copy after upgrade srs lib. 2012-07-19
-#
-# Copy SRS Unlock file from /system partition into /data partition
-#
-#if [ -f /system/data/srsmodels.lic -a ! -f /data/system/srsmodels.lic ]; then 
-#    cat /system/data/srsmodels.lic > /data/system/srsmodels.lic
-#	chmod 775 /data/system/srsmodels.lic
-#fi
 
 #
 # Function to start sensors for DSPS enabled platforms
@@ -57,33 +20,6 @@ start_sensors()
     #fi
     #start sensors
 }
-
-# start ril-daemon only for targets on which radio is present
-#
-baseband=`getprop ro.baseband`
-multirild=`getprop ro.multi.rild`
-dsds=`getprop persist.dsds.enabled`
-netmgr=`getprop ro.use_data_netmgrd`
-case "$baseband" in
-    "msm" | "csfb" | "svlte2a" | "mdm" | "unknown")
-    start ril-daemon
-    start qmuxd
-    case "$baseband" in
-        "svlte2a" | "csfb")
-        start qmiproxy
-    esac
-    case "$multirild" in
-        "true")
-         case "$dsds" in
-             "true")
-             start ril-daemon1
-         esac
-    esac
-    case "$netmgr" in
-        "true")
-        start netmgrd
-    esac
-esac
 
 #
 # Suppress default route installation during RA for IPV6; user space will take
