d6873a755a7a4b583eb0960ef0f78e136dd5cb87
RIP95 <rip95_95@mail.ru>
2014-08-13 19:06:02 +0400

    Fix derp. Change toolchain.

--- envsetup.sh
+++ envsetup.sh
@@ -2,4 +2,4 @@
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=arm-gnueabi-
-export PATH=~/bin:~/bootbuild/zte/linaro-4.6.4-a8/bin:$PATH
+export PATH=~/bin:~/bootbuild/zte/linaro-4.7.4-a15/bin:$PATH
